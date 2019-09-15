c5a970e36680736a93fe9210c687ec3babc5b28b
Franck Michea <franck.michea@gmail.com>
2019-09-15 14:01:03 +0200

    Change from byte to uint8 to keep consistent in type use.

--- pkg/gbc/memory/mmu.go
+++ pkg/gbc/memory/mmu.go
@@ -23,7 +23,7 @@ const (
 )
 
 var (
-	NintendoLogo = []byte(
+	NintendoLogo = []uint8(
 		"\xce\xed\x66\x66\xcc\x0d\x00\x0b\x03\x73\x00\x83\x00\x0c\x00\x0d" +
 			"\x00\x08\x11\x1f\x88\x89\x00\x0e\xdc\xcc\x6e\xe6\xdd\xdd\xd9\x99" +
 			"\xbb\xbb\x67\x63\x6e\x0e\xec\xcc\xdd\xdc\x99\x9f\xbb\xb9\x33\x3e")
@@ -95,7 +95,7 @@ type MMU struct {
 	Title string
 
 	ROMType ROMType
-	ROM     []byte
+	ROM     []uint8
 }
 
 func NewMMU(filename string) (*MMU, error) {
@@ -125,7 +125,7 @@ func (m *MMU) loadRom(filename string) error {
 
 	size := stat.Size()
 
-	m.ROM = make([]byte, size)
+	m.ROM = make([]uint8, size)
 
 	readCount, err := f.Read(m.ROM)
 	if err != nil {

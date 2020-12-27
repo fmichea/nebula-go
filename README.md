ea86b4760ce23c6e4530b7bfc7bc531550afb0a2
Franck Michea <franck.michea@gmail.com>
2020-12-27 20:31:23 +0100

    [main] Add tooling to allow for profiling of the emulator.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 Documentation: http://bgb.bircd.org/pandocs.htm
+
+Profiling:
+
+    PROFILE=$(pwd) $(make run-gbc-cmd) data/bomberman.gb
+    go tool pprof -http localhost:8081 cpu.pprof
--- bins/nebula-gbc-go/main.go
+++ bins/nebula-gbc-go/main.go
@@ -5,11 +5,18 @@ import (
 	"log"
 	"os"
 
+	"github.com/pkg/profile"
 	"nebula-go/pkg/gbc/memory"
 	"nebula-go/pkg/gbc/z80"
 )
 
 func buildAndRunGB(logger io.Writer, filename string) error {
+	profilePath := os.Getenv("PROFILE")
+
+	if profilePath != "" {
+		defer profile.Start(profile.ProfilePath(profilePath)).Stop()
+	}
+
 	mmu, err := memory.NewMMUFromFile(logger, filename)
 	if err != nil {
 		return err
